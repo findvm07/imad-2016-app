5a22312c09ebf9b078099261e8a2955dfeca2b19
findvm07 <luckyvm007@gmail.com>
2016-10-11 12:25:29 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -17,7 +17,7 @@ button.onclick = function () {
   request.send(null);
 };
 
-var submit = getElementById('submit_btn');
+var submit = document.getElementById('submit_btn');
 submit.onclick = function() {
     var request = new XMLHttpRequest();
     request.onreadystatechange = function () {
