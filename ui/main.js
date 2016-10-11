4adbe4375d363101ccf2263a8d78bc10b1f4d255
findvm07 <luckyvm007@gmail.com>
2016-10-11 13:30:05 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -40,3 +40,36 @@ var name = nameInput.value;
 request.open('GET', 'http://findvm07.imad.hasura-app.io/submit-name?name=' + name, true);
   request.send(null);
 };
+
+var submitComment = document.getElementById('submit_btn_art');
+submitComment.onclick = function () {
+    // Make request to server  and send the article
+    // Create a request Object
+    var request = new XMLHttpRequest();
+    
+    // Capture the response and store it in a variable
+    request.onreadystatechange = function () {
+        
+        //Take action
+        if(request.status === 200 ) {
+            
+            // Capture the list of articles and render it as a list.
+            var comments = request.responseText;
+            articles = JSON.parse(comments);
+            var list = '';
+            for(var i=0; i<comments.length; i++) {
+                list += '<li>' + comments[i] + '</li>';
+            }
+            var ul = document.getElementById('articles-para');
+            ul.innerHTML = list;
+        }
+    };
+    
+    //submit comment
+    var commentInput = document.getElementById('comment');
+    var comment = commentvalue;
+    
+    //Make actual reqest 
+    request.open('GET', 'http://findvm07.imad.hasura-app.io/submit-comment?comment=' + comment, true);
+  request.send(null);
+};
